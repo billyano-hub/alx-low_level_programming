bca79e64586426df7770087bedfbb9717939ba05
billyano <billymoney4ever@gmail.com>
2022-09-09 14:47:01 +0100

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the bstring in the put function
+*
+* Description: using the bmain function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like buiolding a multilingual puzzle");    
+	return (0);
+}
