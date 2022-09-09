b45bc280d49a89c60f8ad50c961643595b3eab52
billyano <billymoney4ever@gmail.com>
2022-09-09 16:27:50 +0100

    Sixe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
 *
 * Description: using the main function 
 * this program prints "Programming is like building a multilingual puzzle
-* Return: 0
+* Return: 0 
 */
 int main(void)
 {
@@ -21,4 +21,4 @@ int main(void)
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
-} 
+}
