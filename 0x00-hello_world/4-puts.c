fa5af5590bbfe01b8db848e07aea017fa5e55136
billyano <billymoney4ever@gmail.com>
2022-09-09 14:56:26 +0100

    Push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
-* main - print the bstring in the put function
+* main - print the string in the put function
 *
-* Description: using the bmain function
+* Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
