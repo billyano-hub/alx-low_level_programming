5e1173bed35d902348ed67452ebd62d11cf7cda4
billyano <billymoney4ever@gmail.com>
2022-09-26 18:06:59 +0100

    project

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strcmp - compares 2 strings
+ * @s1: first string
+ * @s2: second string
+ * Return: nothing
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
+Footer
+
